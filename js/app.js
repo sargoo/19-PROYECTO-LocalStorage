18330c4c5fa2e14d8343f8cb8ff13632e866900f
sargoo <goon_aguera@hotmail.com>
2021-12-08 01:09:13 -0300

    se agrega error si se quiere mandar campo vacio

--- js/app.js
+++ js/app.js
@@ -9,6 +9,21 @@ formulario.addEventListener('submit',agregarTweet);
 }
 
 function agregarTweet(e){
-e.preventDefault();
-console.log('agregando tweet');
+    e.preventDefault();
+    const tweet = document.querySelector('#tweet').value;
+
+    if (tweet === '') {
+        mostrarError('el campo no puede estar vacio');
+        return;// evita que se ejecute la linea de abajo si pasa el if
+    }
+    console.log('enviando tweet...');
+    
+}
+
+function mostrarError(error){
+    const mensajeError = document.createElement('p');
+    mensajeError.textContent = error;
+    mensajeError.classList.add('error');
+    const contenido = document.querySelector('#contenido');
+    contenido.appendChild(mensajeError);
 }
